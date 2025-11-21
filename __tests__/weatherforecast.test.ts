bc46fd97a01c31ac9697890f72aeb1599ee25220
Busisile Zilwa <busisile.box@gmail.com>
2025-11-21 19:32:32 +0200

    added tests and readme file

--- __tests__/weatherforecast.test.ts
+++ __tests__/weatherforecast.test.ts
@@ -22,9 +22,40 @@ describe('Weather Forecast Resolver', () => {
         jest.resetAllMocks();
     });
 
+    
+
     it("should fetch weather forecast based on latitude and longitude", async () => {
         (CacheService.get as jest.Mock).mockResolvedValue(null);
         (CacheService.set as jest.Mock).mockResolvedValue(true);
-         
+
+        const daily = [
+            {
+            "date": "2025-11-21",
+            "temp_max": 21.6,
+            "temp_min": 18,
+            "precipitation": 0,
+            "windspeed_max": 44.6,
+            "cloudcover": 8
+            },
+            {
+            "date": "2025-11-22",
+            "temp_max": 23.7,
+            "temp_min": 18.1,
+            "precipitation": 0,
+            "windspeed_max": 46.9,
+            "cloudcover": 41
+            },
+        ];
+        // mock the fetch response
+        (getWeatherForecast as jest.Mock).mockResolvedValue(daily);
+
+        const results = await (resolvers as any).Query.weatherForecast(null, { latitude: -33.9249, longitude: 18.4241, days: 2, timezone: 'auto' });
+        expect(results).toHaveLength(2);
+        expect(results[0].date).toBe("2025-11-21");
+        expect(results[0].temp_max).toBe(21.6);
+        expect(results[0].temp_min).toBe(18);
+        expect(results[0].precipitation).toBe(0);
+        expect(results[0].windspeed_max).toBe(44.6);
+        expect(results[0].cloudcover).toBe(8);
     });
 });
